e250e858947ceea07f7a368072b0c83310145db4
Juan Carlos Vergara <juanvergara9@hotmail.com>
2017-02-14 12:15:33 -0300

    batmanvssuperman

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
-  def batman
-  end
+	
+		def save_user
+			unless params[:name].blank? or params[:email].blank? or params[:eleccion].blank?
+
+				User.create(name:params[:name], email:params[:email], eleccion:params[:eleccion])
+				redirect_to pages_batman_vs_superman_path,
+				notice:"Encuesta enviada correctamente"
+
+			end
+
+		end
+
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ root "pages#batman-vs-superman"
   get 'pages/batman'
   get 'pages/superman'
   get 'batman-vs-superman', to: "pages#batman_vs_superman"
+  post 'pages/save_user'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
