451de2ecd1cbdaa47137aae5644e17e35700b723
Juan Carlos Vergara <juanvergara9@hotmail.com>
2017-02-14 11:37:27 -0300

    batman_vs_superman

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-root 'pages#batman'
+root "pages#batman-vs-superman"
   get 'pages/batman'
   get 'pages/superman'
+  get 'batman-vs-superman', to: "pages#batman_vs_superman"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
